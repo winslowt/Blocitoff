bc65b4cd3dad9fcbf07b40a9f43e68d650fab83a
Tony Winslow <winslowt1@nku.edu>
2015-10-31 15:55:08 -0400

    alcolator finished

---
+++ Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Tony  Winslow on 10/31/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/Alcolator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alcolator
+//
+//  Created by Tony  Winslow on 10/31/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
